b204eedd07b9a8ed6cd6532d2831bc34bece6917
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2022-08-02 22:35:17 -0400

    first rice

---
+++ dotfiles/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
+Footer
+
