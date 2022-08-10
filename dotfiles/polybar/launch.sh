c27abfe81b675a7aef4bcafc205a5880fbf3d8eb
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2022-08-10 13:53:16 -0400

    updates in bspwm and sxkhd

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
